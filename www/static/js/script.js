a201ea84b6ae372c274f435e9e3b6fd410b81598
zhoulin <zhoulin@rd.netease.com>
2019-03-26 15:05:26 +0800

    mod:renew

--- www/static/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    alert("good");
-    $("#login").click(function(){
-        var user = $("#username").val();
-        var pwd = $("#password").val();
-        alert("username: "+user);
-    });
-});
