184f82a11b27ccd15b7c6c0377b18f74a7b0a0d0
zhoulin <zhoulin@rd.netease.com>
2019-03-26 15:33:44 +0800

    mod:renew

---
+++ app/api.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+
+#创建一个服务，赋值给app
+app=Flask(__name__)
+
+#指定接口访问的路径和支持的请求
+@app.route('/HelloWorld',methods=['post','get'])
+#请求后，直接拼接入参方式
+def get_ss():
+    name= '2019'
+    return 'hello world,'+name
+
+app.run(host='127.0.0.1',port=8880,debug=True)
+#windows就一个网卡，可以不写，linux有多个网卡，写成0.0.0.0可以接受任意网卡信息
