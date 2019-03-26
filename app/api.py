13098fb2d4937bb578248b3b2087d6278b4e3cce
zhoulin <zhoulin@rd.netease.com>
2019-03-26 17:11:50 +0800

    mod:renew

--- app/api.py
+++ app/api.py
@@ -10,6 +10,6 @@ def get_ss():
     name= '2019'
     return 'hello world,'+name
 
-app.run(host='127.0.0.1',port=8880,debug=True)
+app.run(host='0.0.0.0',port=8880,debug=True)
 
 #windows就一个网卡，可以不写，linux有多个网卡，写成0.0.0.0可以接受任意网卡信息
